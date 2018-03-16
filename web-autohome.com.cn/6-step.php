6b6e716da252134bf9ae8907f3ea7b2e946f0686
xuweiqiang <435861851@qq.com>
2018-03-16 18:13:53 +0800

    move

---
+++ web-autohome.com.cn/6-step.php
@@ -0,0 +1,16 @@
+<?php
+
+// 加载composer资源库
+require_once('../resources/autoload.php');
+// 初始化数据库配置
+require_once('./lib/config.php');
+// 加载自己项目资源库
+require_once('./lib/sixstep.php');
+// 封装下载类
+require_once('./lib/guzzle.php');
+
+// 建表
+sixstep::initable();
+
+// 分析
+sixstep::move();
