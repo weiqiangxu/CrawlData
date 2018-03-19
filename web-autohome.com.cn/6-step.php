bcf0abf98f146a58464d6d701b1891811e1d0b7a
xuweiqiang <435861851@qq.com>
2018-03-19 10:56:00 +0800

    done

--- web-autohome.com.cn/6-step.php
+++ web-autohome.com.cn/6-step.php
@@ -15,8 +15,11 @@ require_once('./lib/guzzle.php');
 // 建表
 // sixstep::initable();
 
-// 分析
+// 转储数据
 // sixstep::move();
 
 // 检测是否有js类名未转换
-sixstep::echoRejs();
+// sixstep::echoRejs();
+
+// 最终数据检查
+// sixstep::check();
